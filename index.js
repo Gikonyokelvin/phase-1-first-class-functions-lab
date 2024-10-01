b36b4ade29a007d17fc9610d2f82e2f446456be4
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-01 21:53:11 +0300

    complete advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function multipliedFare(fare){
+        return fare * x;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers);
+}
